e4a898ab7711bc1ba1eae6b211e07b5cb7606d83
Kirill <14krabik14@gmail.com>
2021-08-03 16:58:41 +0300

    adder fix

--- ajax/cart.php
+++ ajax/cart.php
@@ -6,7 +6,10 @@
         $cart = $_SESSION['cart'];
         $id = $_POST['id'];
         $newProduct['idProduct'] = $id;
-        $cart[count($cart)] = $newProduct;
+        if (!in_array($newProduct, $cart))
+        {
+            $cart[count($cart)] = $newProduct;
+        }
         $_SESSION['cart'] = $cart;
     }
     function pr(){
